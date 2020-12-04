96fa8f5662bccfb0859af9bfa6ba59f040753e40
stobon <stobon@ufl.edu>
2020-12-04 12:48:04 -0500

    moved agent to footer.php - now persistent on all pages

--- index.php
+++ index.php
@@ -82,18 +82,7 @@
   language-code="en"
 ></df-messenger>
 -->
-<!--- Kommunicate.io --->
-<script type="text/javascript">
-    (function(d, m){
-        var kommunicateSettings = 
-            {"appId":"324ee2c30cd2dfd54664799acd518f4bb","popupWidget":true,"automaticChatOpenOnNavigation":true};
-        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-        window.kommunicate = m; m._globals = kommunicateSettings;
-    })(document, window.kommunicate || {});
-/* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
-</script>
+
 
 </body>
 <!-- Footer -->
