6bf97c5a614d06dcc66f757736240f69e807df00
stobon <stobon@ufl.edu>
2020-12-04 12:43:44 -0500

    new agent

--- index.php
+++ index.php
@@ -73,7 +73,7 @@
 </figure>
 
 
-<!--- Chat-Bot -->
+<!--- Chat-Bot 
 <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
 <df-messenger
   intent="WELCOME"
@@ -81,7 +81,19 @@
   agent-id="96a5049c-6530-4915-a98b-28f2f8f7820b"
   language-code="en"
 ></df-messenger>
-
+-->
+<!--- Kommunicate.io --->
+<script type="text/javascript">
+    (function(d, m){
+        var kommunicateSettings = 
+            {"appId":"324ee2c30cd2dfd54664799acd518f4bb","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+/* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+</script>
 
 </body>
 <!-- Footer -->
