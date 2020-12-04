46c039e12fc122afb65c5fce373ad6fcb6337723
stobon <stobon@ufl.edu>
2020-12-04 16:09:40 -0500

    touchup/img change

--- bed.php
+++ bed.php
@@ -15,14 +15,9 @@
       <div class="col-lg-9">
 
         <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
-          <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          </ol>
           <div class="carousel-inner" role="listbox">
             <div class="carousel-item active">
-              <img class="d-block img-fluid" src="https://images.beallsflorida.com/i/beallsflorida/671-1050-5421-46-yyy?w=650&h=650&img404=404&v=1" alt="First slide">
+              <img class="d-block img-fluid" src="https://www.mattress360.net/wp-content/uploads/2018/09/shutterstock_364522541.jpg" alt="First slide">
             </div>
             <div class="carousel-item">
               <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
@@ -31,14 +26,6 @@
               <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">
             </div>
           </div>
-          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
         </div>
 
         <div class="row">
