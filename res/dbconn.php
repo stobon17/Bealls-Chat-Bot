4c9d19f738d3ebb091261acd4cec653582abd048
Eric Lehmann <katyl@katyl.info>
2020-12-07 14:42:32 -0500

    Consolidate DB Credentials

--- res/dbconn.php
+++ res/dbconn.php
@@ -1,8 +1,9 @@
 <?php
-
-$serverName = "localhost";
-$dbUsername = "root";
-$dbPass = "";
+// IF DEPLOYING VIA DOCKER COMPOSE, VARIABLES ARE SET IN
+// /Bealls-Chat-Bot/.env
+$serverName = "db"; //DO NOT CHANGE
+$dbUsername = ""; // CHANGE TO MATCH MYSQL_USERNAME
+$dbPass = ""; // CHANGE TO MATCH MYSQL_PASSWORD
 $dbName = "admin";
 
 $conn = mysqli_connect($serverName, $dbUsername, $dbPass, $dbName);
